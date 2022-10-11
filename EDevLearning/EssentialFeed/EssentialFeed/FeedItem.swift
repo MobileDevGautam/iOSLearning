e7d4b6a3f783a79c8a97feac939a9240cd60699b
GAUTAM <iamgautamtiwari@gmail.com>
2022-10-11 21:21:39 +0530

    Create the FeedItem Model

---
+++ EDevLearning/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by GAUTAM TIWARI on 11/10/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
